f2e704b33debc7daa3c0e76fadfa9e426d7969df
MatveyAksentev <151757403+MatveyAksentev@users.noreply.github.com>
2025-08-28 18:51:08 +0300

    Update Mavenproject11.java

--- mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
+++ mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject11;
 public class Mavenproject11 {
 
     public static void main(String[] args) {
-        System.out.println("3!");
+        System.out.println("4!");
     }
 }
