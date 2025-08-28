5af10307a65e27390515c8897d2b66c3287d6a19
kwqrina <ekaterina.motruk@gmail.com>
2025-08-28 19:25:06 +0300

    1

--- mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
+++ mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
@@ -8,21 +8,9 @@ package com.mycompany.mavenproject11;
  *
  * @author ekate
  */
-public class HelloWorld {            //мотя люблю тебя
-    public static void main(String[] args) {
-        System.out.println("Привет, мир! ������");
-        System.out.println("Сегодня: " + java.time.LocalDate.now());
-        
-        // Простые вычисления
-        int a = 10;
-        int b = 5;
-        System.out.println(a + " + " + b + " = " + (a + b));
-        
-        // Массив
-        String[] languages = {"Java", "Python", "JavaScript"};
-        System.out.print("Любимые языки: ");
-        for (String lang : languages) {
-            System.out.print(lang + " ");
-        }
-    }
+public class Mavenproject11{
+    
+   public static void main(String[] args) {
+        System.out.println("Привет");
+    }  
 }
