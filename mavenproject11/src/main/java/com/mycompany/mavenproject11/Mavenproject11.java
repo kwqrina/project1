237437a049be70904a772ed6e581b9a8b4fcb7fc
kwqrina <ekaterina.motruk@gmail.com>
2025-08-28 18:45:38 +0300

    1

--- mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
+++ mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject11;
 public class Mavenproject11 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
     }
 }
