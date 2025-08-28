a7e07395b708a44079dd1947b45ac63ca387dd43
MatveyAksentev <151757403+MatveyAksentev@users.noreply.github.com>
2025-08-28 18:31:08 +0300

    2

--- mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
+++ mavenproject11/src/main/java/com/mycompany/mavenproject11/Mavenproject11.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mavenproject2;
+package com.mycompany.mavenproject11;
 
 /**
  *
- * @author macbookair
+ * @author ekate
  */
-public class Mavenproject2 {
+public class Mavenproject11 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
